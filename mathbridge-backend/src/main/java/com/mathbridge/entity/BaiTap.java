0b555078d7e228488fb1876b2f1f74cc5b824c48
TONYNNN123 <156822469+TONYNNN123@users.noreply.github.com>
2025-11-23 23:32:37 +0700

    UPDATE

--- mathbridge-backend/src/main/java/com/mathbridge/entity/BaiTap.java
+++ mathbridge-backend/src/main/java/com/mathbridge/entity/BaiTap.java
@@ -38,12 +38,6 @@ public class BaiTap {
     @Column(name = "TaiLieuURL", length = 400)
     private String taiLieuUrl;
 
-    @Column(name = "ChoPhepLamBai")
-    private Boolean choPhepLamBai;
-
-    @Column(name = "HocSinhDuocPhep", length = 1000)
-    private String hocSinhDuocPhep; // JSON array of student IDs: ["HS001", "HS002"]
-
     @Column(name = "NgayBatDau", nullable = false)
     private LocalDateTime ngayBatDau;
 
@@ -53,31 +47,15 @@ public class BaiTap {
     @Column(name = "GhiChu", length = 200)
     private String ghiChu;
 
-    @Lob
-    @Column(name = "NoiDungJSON")
-    private String noiDungJson;
+    @Column(name = "ChoPhepLamBai")
+    private Boolean choPhepLamBai;
 
-    @Column(name = "CanhBao", length = 500)
-    private String canhBao;
-
-    @Column(name = "ThoiLuongPhut")
-    private Integer thoiLuongPhut;
-
-    // Removed: TuDongNop and CheDoChamDiem - these fields exist in BaiNop table
-    // @Column(name = "TuDongNop")
-    // private Boolean tuDongNop;
-    // @Column(name = "CheDoChamDiem", length = 50)
-    // private String cheDoChamDiem;
-
-    // QUAN HỆ
+    @Column(name = "HocSinhDuocPhep", length = 1000)
+    private String hocSinhDuocPhep;
 
     @ManyToOne
     @JoinColumn(name = "ID_BH", insertable = false, updatable = false)
     private BuoiHocChiTiet buoiHocChiTiet;
-
-    // Removed: @OneToOne relationship with BaiTapNoiDung - data now in BaiTap table
-    // @OneToOne(mappedBy = "baiTap", cascade = CascadeType.ALL, orphanRemoval = true)
-    // private BaiTapNoiDung noiDungChiTiet;
 
     @OneToMany(mappedBy = "baiTap")
     private Set<BaiNop> baiNops = new HashSet<>();
